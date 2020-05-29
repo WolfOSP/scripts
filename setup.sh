0a157c9eaea9c68763b6b17f809dd838a47a4275
VirajKanse <virajv2k@gmail.com>
2020-05-29 21:02:22 +0530

    :gear: adapt to proton clang

--- setup.sh
+++ setup.sh
@@ -1,19 +1,17 @@
 git config --global user.name "WolfOSP"
 git config --global user.email "WolfOSP@outlook.com"
 cd $HOME
+unset BOT_API_KEY
+unset CHAT_ID
 export KERNELDIR=$HOME/kernel_asus_sdm660-HMP
 export KERNELNAME=WOLF-HMP-TEST
 sed -i '$d' $HOME/.bashrc
 install-package ccache bc libncurses5-dev git-core gnupg flex bison gperf build-essential zip curl libc6-dev ncurses-dev && echo 'export PATH="/usr/lib/ccache:$PATH"' | tee -a ~/.bashrc &&source ~/.bashrc && echo $PATH
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 toolchain32
-git clone https://github.com/crdroidmod/android_prebuilts_clang_host_linux-x86_clang-6317467.git clang
-wget https://releases.linaro.org/components/toolchain/binaries/latest-7/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
-tar -xvf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
-rm gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
+git clone --depth 1 https://github.com/kdrag0n/proton-clang.git clang
+export PATH="$HOME/clang/bin:$PATH"
 rm -rf $HOME/{.phpbrew,.rbenv}
 cd kernel_asus_sdm660-HMP
 git clone https://github.com/WolfOSP/AnyKernel3.git
-curl https://raw.githubusercontent.com/WolfOSP/scripts/hmp/changelog-generator.sh -o changelog-generator.sh
-curl https://raw.githubusercontent.com/WolfOSP/scripts/hmp/functions -o functions
-curl https://raw.githubusercontent.com/WolfOSP/scripts/hmp/wolf.sh -o wolf.sh
+curl https://raw.githubusercontent.com/WolfOSP/scripts/proton/functions -o functions
+curl https://raw.githubusercontent.com/WolfOSP/scripts/proton/wolf.sh -o wolf.sh
 #bash wolf.sh
--- wolf.sh
+++ wolf.sh
@@ -4,12 +4,11 @@
 
 source "${KERNELDIR}/functions"
 
-export TZ="UTC";
+export TZ="Asia/Kolkata";
 
 
 ##################################################
-#            Begin Initial Checks                #
-##################################################
+## Begin Initial Checks
 
 check_var CHAT_ID
 check_var BOT_API_KEY
@@ -17,15 +16,7 @@ check_var KERNELNAME
 check_var KERNELDIR
 
 ##################################################
-#               End Initial Checks               #
-##################################################
-
-
-##################################################
-#               envSetup variables               #
-##################################################
-#             CHANGEABLE PARAMETERS              #
-##################################################
+## Env Setup variables | CHANGEABLE PARAMETERS             
 
 export SRCDIR="${KERNELDIR}";
 export OUTDIR="${KERNELDIR}/out";
@@ -36,34 +27,22 @@ export SUBARCH="arm64";
 export KBUILD_BUILD_USER="Wolf"
 export KBUILD_BUILD_HOST="WolfOSP"
 export MAKE_TYPE="HMP(non-SAR)"
-export TOOLCHAIN="${HOME}/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/";
 export DEFCONFIG="X00T_defconfig";
 export ZIP_DIR="$ANYKERNEL";
 export IMAGE="${OUTDIR}/arch/${ARCH}/boot/Image.gz-dtb";
-export CROSS_COMPILE_ARM32=${HOME}/toolchain32/bin/arm-linux-androideabi-
-export CC=$HOME/clang/bin/clang
-export CLANG_VERSION=$($CC --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export CLANG_LD_PATH=$HOME/clang/lib
-export LLVM_DIS=$HOME/clang/bin/llvm-dis
+export CLANG_VERSION=$(clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 export JOBS="$(nproc --all)"
 export ZIPNAME="${KERNELNAME}-${DEVICE}-$(date +%Y%m%d-%H%M).zip"
 export FINAL_ZIP="${ZIP_DIR}/${ZIPNAME}"
+export MAKE="make O=${OUTDIR}";
 
 wolf_version=$(grep "^CONFIG_LOCALVERSION" arch/arm64/configs/X00T_defconfig | cut -d "=" -f2 | tr -d '"')
 tag=$(head -n3 Makefile | sed -E 's/.*(^\w+\s[=]\s)//g' | xargs | sed -E 's/(\s)/./g')
-##################################################
-#        DO NOT CHANGE BEYOND THIS POINT         #
-##################################################
-
 
 ##################################################
-#          Begin Compilation Process             #
-##################################################
+## Begin Compilation Process
 
-export MAKE="make O=${OUTDIR}";
-
-check_toolchain;
+#check_toolchain;
 
 [ ! -d "${ZIP_DIR}" ] && mkdir -pv ${ZIP_DIR}
 [ ! -d "${OUTDIR}" ] && mkdir -pv ${OUTDIR}
@@ -74,11 +53,10 @@ rm -fv ${IMAGE};
 MAKE_STATEMENT=make
 
 ###################################################
-#            Menuconfig configuration             #
-###################################################
+## Menuconfig configuration
+## If -no-menuconfig flag is present we will skip the kernel configuration step.
+## Make operation will use X00T_defconfig directly.
 
-# If -no-menuconfig flag is present we will skip the kernel configuration step.
-# Make operation will use X00T_defconfig directly.
 if [[ "$*" == *"-no-menuconfig"* ]]
 then
   NO_MENUCONFIG=1
@@ -96,30 +74,41 @@ fi
 send_msg "<code>Build scheduled for $KERNELNAME kernel</code>"
 
 ${MAKE} $DEFCONFIG;
+
 START=$(date +"%s");
+
 echo -e "Using ${JOBS} threads to compile"
+
 ${MAKE} -j${JOBS} \
   	      ARCH="$ARCH" \
-              CC=${HOME}/clang/bin/clang \
-              CROSS_COMPILE="${CROSS_COMPILE}" \
-              CLANG_TRIPLE="${CROSS_COMPILE}" |& tee build-log.txt
+              CC=clang \
+              CROSS_COMPILE=aarch64-linux-gnu- \
+              CROSS_COMPILE_ARM32=arm-linux-gnueabi- |& tee build-log.txt
 exitCode="$?";
 END=$(date +"%s")
 DIFF=$(($END - $START))
 time="$(($DIFF / 60))m $(($DIFF % 60))s"
 echo -e "$time"
 
-bash changelog-generator.sh | tee changelog.txt >/dev/null
+changelog-generator | tee changelog.txt >/dev/null
 buildlog=$(deldog build-log.txt)
 changelog=$(deldog changelog.txt)
 
+##################################################
+## validate the build
+
 if [[ ! -f "${IMAGE}" ]]; then
     echo -e "Build failed :P";
-    send_msg "<code>$KERNELNAME kernel stopped due to an error</code>"
+    send_msg "<code>$KERNELNAME kernel stopped due to an error</code>
+<b>Build Log:</b>
+<a href='$buildlog'>build log</a>"
     exit 1;
 else
     echo -e "Build Succesful!";
 fi
+
+##################################################
+## zip creation
 
 echo -e "Copying kernel image";
 cp -v "${IMAGE}" "${ANYKERNEL}/";
@@ -130,13 +119,7 @@ zip -r9 ${FINAL_ZIP} * -x .git -x README.md;
 cd -;
 
 ##################################################
-#          Finish Compilation Process            #
-##################################################
-
-
-##################################################
-#             Upload the build                   #
-##################################################
+## Upload the build
 
 if [ -f "$FINAL_ZIP" ]
 then
